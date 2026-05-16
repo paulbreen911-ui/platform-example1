43b3c3ca263749bb735eec19092529725cb0a5ca
paulbreen911-ui <paulbreen911@gmail.com>
2026-05-16 15:17:37 -0700

    Update logout.php

--- user/logout.php
+++ user/logout.php
@@ -1,10 +1,6 @@
 <?php
 
-$page_title = 'Log Out';
-
 require_once __DIR__ . '/../config.php';
-require_once ROOT_PATH . '/functions.php';
-include ROOT_PATH . '/required/header.php';
 
 $_SESSION = [];
 session_destroy();
