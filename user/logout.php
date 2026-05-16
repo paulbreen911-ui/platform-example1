7b25305df686dd9cb3454b308ae54d882cc36a22
paulbreen911-ui <paulbreen911@gmail.com>
2026-05-16 15:16:20 -0700

    Refactor logout.php to include functions and header

--- user/logout.php
+++ user/logout.php
@@ -1,5 +1,10 @@
 <?php
-require_once '../config.php';
+
+$page_title = 'Log Out';
+
+require_once __DIR__ . '/../config.php';
+require_once ROOT_PATH . '/functions.php';
+include ROOT_PATH . '/required/header.php';
 
 $_SESSION = [];
 session_destroy();
